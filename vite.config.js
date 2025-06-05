dd5b4653f548e2e3924c78eecc74b58b2289e7a1
bloggerlucky43 <bloggerlucky43@gmail.com>
2025-06-05 22:37:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   
   server: {
-    allowedHosts: ['c408-102-89-83-10.ngrok-free.app'],
+    allowedHosts: ['https://depeaceacademy.onrender.com','https://depeacebackend.onrender.com'],
   }
 })
